09a1d68156b13a69ebdd56d37e8ebb3c64c4cd89
Yulia Isakova <gunoffun@yandex.ru>
2023-05-22 13:00:39 +0300

    update render

--- src/view.js
+++ src/view.js
@@ -1,106 +1,106 @@
 import onChange from 'on-change';
 
+const renderState = (value, watchedState, i18n, elements) => {
+  if (value === 'invalid') {
+    const { error } = watchedState;
+    elements.input.classList.add('is-invalid');
+    elements.feedback.classList.remove('text-danger');
+    elements.feedback.classList.remove('text-success');
+    elements.feedback.classList.add('text-danger');
+    elements.feedback.textContent = i18n.t(`${error.url.message}`);
+  }
+  if (value === 'valid') {
+    elements.input.classList.remove('is-invalid');
+    elements.feedback.classList.remove('text-danger');
+    elements.feedback.classList.remove('text-success');
+    elements.feedback.classList.add('text-success');
+    elements.feedback.textContent = i18n.t('success');
+    elements.form.reset();
+  }
+  if (value === 'fall') {
+    elements.input.classList.remove('is-invalid');
+    elements.feedback.classList.remove('text-danger');
+    elements.feedback.classList.remove('text-success');
+    elements.feedback.classList.add('text-danger');
+    elements.feedback.textContent = i18n.t('fall');
+  }
+  if (value === 'networkError') {
+    elements.input.classList.remove('is-invalid');
+    elements.feedback.classList.remove('text-danger');
+    elements.feedback.classList.remove('text-success');
+    elements.feedback.classList.add('text-danger');
+    elements.feedback.textContent = i18n.t('networkError');
+  }
+};
+
+const renderFeeds = (watchedState, elements) => {
+  const h2 = document.createElement('h2');
+  h2.classList.add('card-title', 'h4');
+  h2.textContent = 'Фиды';
+  elements.headerFeeds.innerHTML = '';
+  elements.headerFeeds.append(h2);
+
+  elements.listFeeds.innerHTML = '';
+  watchedState.feeds.forEach((feed) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const h3 = document.createElement('h3');
+    h3.classList.add('h-6', 'm-0');
+    h3.textContent = feed.title;
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = feed.description;
+    li.append(p);
+    li.prepend(h3);
+    elements.listFeeds.prepend(li);
+  });
+};
+
+const renderPosts = (watchedState, elements) => {
+  elements.headerPosts.innerHTML = '';
+  const h2Posts = document.createElement('h2');
+  h2Posts.classList.add('card-title', 'h4');
+  h2Posts.textContent = 'Посты';
+  elements.headerPosts.append(h2Posts);
+
+  elements.listPosts.innerHTML = '';
+  watchedState.posts.forEach((post) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    const a = document.createElement('a');
+    a.setAttribute('href', post.url);
+    if (watchedState.uiState.viewedPosts.indexOf(post.id.toString()) === -1) {
+      a.classList.add('fw-bold');
+    } else {
+      a.classList.add('fw-normal', 'link-secondary');
+    }
+    a.setAttribute('data-id', post.id);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.textContent = post.title;
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.setAttribute('data-id', post.id);
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#modal');
+    button.textContent = 'Просмотр';
+    li.append(a);
+    li.append(button);
+    elements.listPosts.prepend(li);
+  });
+};
+
 const render = (state, i18nInstance, elements) => {
-  const renderState = (value, watchedState, i18n) => {
-    if (value === 'invalid') {
-      const { error } = watchedState;
-      elements.input.classList.add('is-invalid');
-      elements.feedback.classList.remove('text-danger');
-      elements.feedback.classList.remove('text-success');
-      elements.feedback.classList.add('text-danger');
-      elements.feedback.textContent = i18n.t(`${error.url.message}`);
-    }
-    if (value === 'valid') {
-      elements.input.classList.remove('is-invalid');
-      elements.feedback.classList.remove('text-danger');
-      elements.feedback.classList.remove('text-success');
-      elements.feedback.classList.add('text-success');
-      elements.feedback.textContent = i18n.t('success');
-      elements.form.reset();
-    }
-    if (value === 'fall') {
-      elements.input.classList.remove('is-invalid');
-      elements.feedback.classList.remove('text-danger');
-      elements.feedback.classList.remove('text-success');
-      elements.feedback.classList.add('text-danger');
-      elements.feedback.textContent = i18n.t('fall');
-    }
-    if (value === 'networkError') {
-      elements.input.classList.remove('is-invalid');
-      elements.feedback.classList.remove('text-danger');
-      elements.feedback.classList.remove('text-success');
-      elements.feedback.classList.add('text-danger');
-      elements.feedback.textContent = i18n.t('networkError');
-    }
-  };
-
-  const renderFeeds = (watchedState) => {
-    const h2 = document.createElement('h2');
-    h2.classList.add('card-title', 'h4');
-    h2.textContent = 'Фиды';
-    elements.headerFeeds.innerHTML = '';
-    elements.headerFeeds.append(h2);
-
-    elements.listFeeds.innerHTML = '';
-    watchedState.feeds.forEach((feed) => {
-      const li = document.createElement('li');
-      li.classList.add('list-group-item', 'border-0', 'border-end-0');
-      const h3 = document.createElement('h3');
-      h3.classList.add('h-6', 'm-0');
-      h3.textContent = feed.title;
-      const p = document.createElement('p');
-      p.classList.add('m-0', 'small', 'text-black-50');
-      p.textContent = feed.description;
-      li.append(p);
-      li.prepend(h3);
-      elements.listFeeds.prepend(li);
-    });
-  };
-
-  const renderPosts = (watchedState) => {
-    elements.headerPosts.innerHTML = '';
-    const h2Posts = document.createElement('h2');
-    h2Posts.classList.add('card-title', 'h4');
-    h2Posts.textContent = 'Посты';
-    elements.headerPosts.append(h2Posts);
-
-    elements.listPosts.innerHTML = '';
-    watchedState.posts.forEach((post) => {
-      const li = document.createElement('li');
-      li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-      const a = document.createElement('a');
-      a.setAttribute('href', post.url);
-      if (watchedState.uiState.viewedPosts.indexOf(post.id.toString()) === -1) {
-        a.classList.add('fw-bold');
-      } else {
-        a.classList.add('fw-normal', 'link-secondary');
-      }
-      a.setAttribute('data-id', post.id);
-      a.setAttribute('target', '_blank');
-      a.setAttribute('rel', 'noopener noreferrer');
-      a.textContent = post.title;
-      const button = document.createElement('button');
-      button.setAttribute('type', 'button');
-      button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-      button.setAttribute('data-id', post.id);
-      button.setAttribute('data-bs-toggle', 'modal');
-      button.setAttribute('data-bs-target', '#modal');
-      button.textContent = 'Просмотр';
-      li.append(a);
-      li.append(button);
-      elements.listPosts.prepend(li);
-    });
-  };
-
   const watchedState = onChange(state, (path, value) => {
     if (path === 'status') {
-      renderState(value, state, i18nInstance);
+      renderState(value, state, i18nInstance, elements);
     }
     if (path === 'feeds') {
-      renderFeeds(state);
+      renderFeeds(state, elements);
     }
     if (path === 'posts') {
-      renderPosts(state);
+      renderPosts(state, elements);
     }
   });
 
